00a5d2660b63f8faba4e80c843b2f18321b4b13a
sonysm <sonysum7@gmail.com>
2026-07-13 15:05:53 +0700

    Include credentials on login fetch

--- src/app/login/login-form.tsx
+++ src/app/login/login-form.tsx
@@ -59,6 +59,7 @@ export default function LoginForm() {
         try {
             const response = await fetch("/api/auth/login", {
                 method: "POST",
+                credentials: "include",
                 headers: {
                     "Content-Type": "application/json",
                 },
